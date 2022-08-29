5222cf2faf2e52a630cb6426dba27df30fb78cb2
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-08-29 16:54:45 -0300

    pagina da web

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-const inputTarefa = document.getElementById ("tarefa-nova")
+const inputTarefa = document.getElementById("tarefa-nova")
+function adicionarTarefa() {
+    const item = document.createElement("li");
+
+    
+    item.innerHTML = `<li>
+   <span>${inputTarefa.value}</span>
+   <button class="excluir">x</button>
+
+</li>`;
+
+    const lista = document.querySelector("#lista");
+    lista.appendChild(item);
+
+    inputTarefa.value =""
+}
+
+const botaoAdicionar = document.querySelector("#btn-adicionar")
+botaoAdicionar.addEventListener("click", adicionarTarefa)
+
+function LimparLista() {
+    const lista = document.querySelector("#lista");
+ lista.innerHTML = ""
+}
+ const botaoLimpar = document.querySelector(".bt-azul")
+
+ botaoLimpar.addEventListener("click", LimparLista)
+
